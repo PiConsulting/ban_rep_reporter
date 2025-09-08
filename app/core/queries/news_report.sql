555bb195e568e002fd80deaf7a0961ec0f005030
Jesus Rojas <jesus.rojas@piconsulting.com.ar>
2025-09-08 16:30:21 -0500

    [FIX] Query de reporte

--- app/core/queries/news_report.sql
+++ app/core/queries/news_report.sql
@@ -1,16 +1,15 @@
 SELECT
-    TOP (100)
-    exe.publish_date AS 'Fecha de publicación',
-    exe.economic_activity AS 'Actividad económica',
-    exe.new_url AS 'URL Noticia',
-    (SELECT news_outlets.new_outlet_name FROM news_outlets WHERE news_outlets.id=exe.new_outlet_id) AS 'Medio de Comunicación',
-    region AS 'Cobertura Geográfica',
-    exe.new_title AS 'Titulo',
-    exe.new_subtitle AS 'Subtitulo',
-    exe.summary AS 'Resumen',
-    exe.topics AS 'Topicos',
-    exe.news_of_interest AS 'Noticia de interes',
-    exe.economic_activity AS 'Actividad Economica'
+     TOP (100)
+        exe.publish_date AS 'Fecha de publicación',
+        exe.economic_activity AS 'Actividad económica',
+        exe.new_url AS 'Enlace Web',
+        (SELECT news_outlets.new_outlet_name FROM news_outlets WHERE news_outlets.id=exe.new_outlet_id) AS 'Medio de comunicación',
+        region AS 'Cobertura geográfica',
+        (exe.new_title + ' \n\n ' + exe.new_subtitle) AS 'Título y subtítulo',
+        exe.summary AS 'Resumen',
+        exe.topics AS 'Tópicos',
+        exe.news_of_interest AS 'Noticia de interés',
+        exe.economic_activity AS 'Actividad económica'
 FROM
     executions as exe
 WHERE
